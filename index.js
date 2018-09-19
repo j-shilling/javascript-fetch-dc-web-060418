422826ab7731936a999f6da77d4d4665bd953102
j-shilling <shilling.jake@gmail.com>
2018-09-19 15:47:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const app = "I don't do much.";
+(function() {
+	const token = '3d2d1ca08030190ed70a9832bcd1536c721f5909';
+	fetch('https://api.github.com/user/repos', {
+	  headers: {
+	    Authorization: `token ${token}`
+	  }
+	}).then(res => res.json()).then(json => console.log(json));
+})()
